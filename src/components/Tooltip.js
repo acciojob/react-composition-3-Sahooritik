850d05d632f3bb44464a0d378e0720d45baea3d8
Ritik Sahoo <ritiksahoo380@gmail.com>
2026-04-26 14:31:52 +0530

    p2

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,36 +3,37 @@ import React, { useState } from "react";
 const Tooltip = ({ text, children }) => {
   const [showTooltip, setShowTooltip] = useState(false);
 
-  return (
-    <div
-      className="tooltip"
-      onMouseEnter={() => setShowTooltip(true)}
-      onMouseLeave={() => setShowTooltip(false)}
-      style={{ position: "relative", display: "inline-block" }}
-    >
-      {children}
+  return React.cloneElement(children, {
+    className: "tooltip",
+    onMouseEnter: () => setShowTooltip(true),
+    onMouseLeave: () => setShowTooltip(false),
+    style: { position: "relative" },
+    children: (
+      <>
+        {children.props.children}
 
-      {showTooltip && (
-        <span
-          className="tooltiptext"
-          style={{
-            position: "absolute",
-            bottom: "125%",
-            left: "50%",
-            transform: "translateX(-50%)",
-            backgroundColor: "tomato",
-            color: "#fff",
-            padding: "5px 10px",
-            borderRadius: "5px",
-            whiteSpace: "nowrap",     
-            zIndex: 1,
-          }}
-        >
-          {text}
-        </span>
-      )}
-    </div>
-  );
+        {showTooltip && (
+          <div
+            className="tooltiptext"
+            style={{
+              position: "absolute",
+              bottom: "125%",
+              left: "50%",
+              transform: "translateX(-50%)",
+              backgroundColor: "tomato",
+              color: "#fff",
+              padding: "5px 10px",
+              borderRadius: "5px",
+              whiteSpace: "nowrap",
+              zIndex: 1,
+            }}
+          >
+            {text}
+          </div>
+        )}
+      </>
+    ),
+  });
 };
 
 export default Tooltip;
